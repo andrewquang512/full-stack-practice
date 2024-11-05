54116ff06cb7f583cb505204a9e9e40da5d0d8e3
andrewquang512 <andrew.viquang@gmail.com>
2024-11-06 00:32:32 +0700

    fix(boilerplate): update Dockerfile

--- boilerplate/frontend/vite.config.ts
+++ boilerplate/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import commonjs from "vite-plugin-commonjs"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
